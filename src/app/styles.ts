1e9096c7bf40b0ad8867bf3871e10d9e13ccb6f9
djonatanabreu <djontan.al@live.com>
2024-03-19 23:27:56 -0300

    fix&feat:cleaning project tree ♻️; add styled-components

---
+++ src/app/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const BasicComponents = {
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    background-color: black;
+    padding: 6rem;
+    min-height: 100vh;
+  `,
+  Title: styled.h1`
+    color: white;
+  `,
+};
